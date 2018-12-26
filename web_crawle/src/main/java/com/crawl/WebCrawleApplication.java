48491f43a35736056e2591f0999749acc9f642e9
wangfan <tianlianyouren@163.com>
2018-12-26 15:35:12 +0800

    crawle without maven

--- web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
+++ web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
@@ -3,17 +3,17 @@ package com.crawl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@EnableEurekaClient
 @ComponentScan
 @MapperScan(value= {"com.crawl.data.*.dao","com.sky"})
 public class WebCrawleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebCrawleApplication.class, args);
+		 SpringApplication.run(WebCrawleApplication.class, args);
 	}
 
 }
