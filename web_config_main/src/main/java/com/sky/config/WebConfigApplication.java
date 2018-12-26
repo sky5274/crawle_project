fee666da7e41af0c01861601b1817a6158b54d1d
wangfan <tianlianyouren@163.com>
2018-12-26 09:54:44 +0800

    spring 服务中心

--- web_config_main/src/main/java/com/sky/config/WebConfigApplication.java
+++ web_config_main/src/main/java/com/sky/config/WebConfigApplication.java
@@ -2,12 +2,10 @@ package com.sky.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class WebConfigApplication {
 
 	public static void main(String[] args) {
