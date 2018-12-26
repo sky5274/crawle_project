2a5b1fdbd7462e25c3db6ea7c0d708970323e87c
wangfan <tianlianyouren@163.com>
2018-12-26 09:46:26 +0800

    crawle project 模块适配调整

--- web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
+++ web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
@@ -1,14 +1,15 @@
 package com.crawl;
 
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan
-@MapperScan(value= {"com.ceawl.data.*.dao","com.sky"})
+@MapperScan(value= {"com.crawl.data.*.dao","com.sky"})
 public class WebCrawleApplication {
 
 	public static void main(String[] args) {
