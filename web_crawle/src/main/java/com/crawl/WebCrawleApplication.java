bb181169b5f3a3f0fa598be8dfcb2a0e8974fc28
wangfan <tianlianyouren@163.com>
2018-12-29 10:56:37 +0800

    spring cloud  修改配置

--- web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
+++ web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
@@ -3,9 +3,11 @@ package com.crawl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(value= {"com.sky","com.crawl"})
 @MapperScan(value= {"com.crawl.data.*.dao"})
 public class WebCrawleApplication {
