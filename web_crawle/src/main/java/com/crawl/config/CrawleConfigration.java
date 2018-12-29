09395d7083197fcceb60bd993db43baa7a626746
wangfan <tianlianyouren@163.com>
2018-12-29 10:33:39 +0800

    添加系统校验配置对象

---
+++ web_crawle/src/main/java/com/crawl/config/CrawleConfigration.java
@@ -0,0 +1,23 @@
+package com.crawl.config;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * crawl  配置
+ * @author 王帆
+ * @date  2018年12月26日 下午4:50:12
+ */
+@Configuration
+public class CrawleConfigration {
+	
+	@Bean
+	public Validator getValidation() {
+		ValidatorFactory vf = Validation.buildDefaultValidatorFactory();
+       return vf.getValidator();
+	}
+}
