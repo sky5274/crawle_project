d6f15dbb4ee2342131f2deaacb4f1dd2f80d99e2
wangfan <tianlianyouren@163.com>
2018-12-29 10:23:23 +0800

    base-dev springboot  配置修改

--- web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
+++ web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
@@ -3,18 +3,16 @@ package com.crawl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@ComponentScan
-@MapperScan(value= {"com.crawl.data.*.dao","com.sky"})
+@ComponentScan(value= {"com.sky","com.crawl"})
+@MapperScan(value= {"com.crawl.data.*.dao"})
 public class WebCrawleApplication {
 
 	public static void main(String[] args) {
 		 SpringApplication.run(WebCrawleApplication.class, args);
 	}
-
+	
 }
 
