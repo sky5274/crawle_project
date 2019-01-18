3fdd1383e1cde6bdd4b4101d03b238a5577ea4e4
wangfan <tianlianyouren@163.com>
2019-01-18 09:17:13 +0800

    页面添加全局url前缀信息

---
+++ core_pub/src/main/java/com/sky/pub/common/SessionInterceptor.java
@@ -0,0 +1,33 @@
+package com.sky.pub.common;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SessionInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+			String url=request.getRequestURL().toString();
+			request.getSession().setAttribute("contextPath",url.substring(0,url.indexOf(request.getServletPath())));
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
