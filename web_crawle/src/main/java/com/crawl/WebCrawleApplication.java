7327b91956727bddb6983973eff0d9435fcd2e43
wangfan <tianlianyouren@163.com>
2019-01-19 22:07:10 +0800

    spring boot  单体应用调整

--- web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
+++ web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
@@ -3,11 +3,9 @@ package com.crawl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaClient
 @ComponentScan(value= {"com.sky","com.crawl"})
 @MapperScan(value= {"com.crawl.data.*.dao"})
 public class WebCrawleApplication {
