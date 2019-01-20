f1dd6d0c31e29c179f97072b17387177d882e840
wangfan <tianlianyouren@163.com>
2019-01-20 10:30:22 +0800

    config  table  sql 修改

--- web_crawle/src/main/java/com/crawl/controller/CrawlerController.java
+++ web_crawle/src/main/java/com/crawl/controller/CrawlerController.java
@@ -2,15 +2,12 @@ package com.crawl.controller;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,7 +33,6 @@ import com.sky.pub.ResultUtil;
 import com.sky.pub.common.exception.ResultException;
 
 import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
-import groovy.util.logging.Slf4j;
 
 /**
  * 爬虫控制器
