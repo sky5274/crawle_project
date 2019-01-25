7dbc0128fc509d94af88f0a1117f88ae3b8ccb74
wangfan <tianlianyouren@163.com>
2019-01-25 10:20:30 +0800

    爬虫配置修改与卡片页修改

--- web_crawle/src/main/resources/static/js/config/crawler.page.exc.js
+++ web_crawle/src/main/resources/static/js/config/crawler.page.exc.js
@@ -2,31 +2,36 @@
 function getConfigData(obj){
 	var data={}
 	var isValid=true;
-	console.log(obj)
-	$(obj.table_ele).find("input,select,textarea").each(function(){
-		if($(this).attr("require")=="true"){
-			if($(this).val()==""){
+	$(obj.table_ele).find("input:visible,select:visible,textarea:visible").each(function(){
+		if($(this).val()!=""){
+			var type=$(this).attr("type")
+			if(type=="checkbox" || type=="radio"){
+				if($(this).prop("checked")){
+					data[$(this).attr("name")]=$(this).val()
+				}
+			}else{
+				data[$(this).attr("name")]=$(this).val();	
+			}
+		}else{
+			if($(this).attr("require")=="true"){
 				isValid=false;
 				$.alert({type:"warn",content:$(this).parent().text()+"不能为空"})
 			}
 		}
-		var type=$(this).attr("type")
-		if(type=="checkbox" || type=="radio"){
-			if($(this).prop("checked")){
-				data[$(this).attr("name")]=$(this).val()
-			}
-		}else{
-			data[$(this).attr("name")]=$(this).val();	
-		}
+		
 	})
 	$(obj.attr_ele).each(function(){
 		var attr={}
 		$(this).find("input,select").each(function(){
 			var type=$(this).attr("type")
 			if(type=="checkbox" || type=="radio"){
-				if($(this).prop("checked")){
-					attr[$(this).attr("name")]=$(this).val()
+				try {
+					if($(this).prop("checked")){
+						attr[$(this).attr("name")]=$(this).val()
+					}
+				} catch (e) {
 				}
+				
 			}else{
 				var value=$(this).val();
 				attr[$(this).attr("name")]=value;
@@ -44,38 +49,85 @@ function getConfigData(obj){
 		return data;
 	}	
 }
-var loop 
-function listenLog(data,node){
-	loop=window.setInterval(function(){
-		listenLogContent(data,node)
-	},1000)
+var loop =true;
+function listenLog(data,node,obj){
+	$(".log-content").html("")
+	loop=true;
+	listenLogContent(data,node,obj)
 }
-/**动态查询爬虫临时日志*/
-function listenLogContent(data,node){
-	$.doJsonAjax({
-		url:"/crawle/config/log/defined",
-		data:{id:data.id},
-		ignore:true,
-		success:function(res){
-			if(res.success){
-				if(res.data.status==4){
+/**
+ * 1:动态查询爬虫临时日志
+ * 2:使用递归形式查询临时日志，减少过度频繁的调度与时序错乱
+ * */
+function listenLogContent(data,node,obj){
+	if(loop){
+		window.setTimeout(function(){
+			$.doJsonAjax({
+				url:"/crawle/config/log/defined",
+				data:{id:data.id},
+				ignore:true,
+				loadding:true,
+				loadtarget:$(".tab-content").find(".log-content").parents(".tab-pane"),
+				success:function(res){
+					if(res.success){
+						if(res.data.status==4){
+							clearBtnStatus(node)
+						}else{
+							listenLogContent(data,node,obj)
+						}
+						if(res.data){
+							showCrawlerLog(res.data,obj)
+						}
+					}else{
+						clearBtnStatus(node)
+					}
+				},
+				error:function(){
 					clearBtnStatus(node)
 				}
-				console.log(res)
-			}else{
-				clearBtnStatus(node)
-			}
-		},
-		error:function(){
-			clearBtnStatus(node)
-		}
-	})
+			})
+		},5000)
+	}
+}
+
+function showCrawlerLog(data,obj){
+	var log_title=$(obj.panel).find(".nav-tabs").children("li#crawler_log_title");
+	if(log_title.length==0){
+		log_title=addCrawlerLogPanel(data,obj)
+	}
+	var panle_id=$(log_title).find("a").attr("href")
+//	var con=data.context.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+	$(panle_id).find(".log-content").append(data.context)
+	targgleTab(log_title);
+}
+function targgleTab(node){
+	$(node).parent().find(".active").removeClass("active")
+	$(node).addClass("active")
+	var tab_content=$($(node).children("a").attr("href"))
+	tab_content.parent().find(".active").removeClass("active")
+	tab_content.addClass("active")
+}
+
+/**添加爬虫日志面板*/
+function addCrawlerLogPanel(data,obj){
+	var panel_id="crawler-log-content"
+	var li=$('<li id="crawler_log_title" class="pos-relative"><a href="#'+panel_id+'" data-toggle="tab">爬虫日志</a><span style=" position: absolute; top: 0; right: 5px; cursor: pointer;" onClick="closeLogPanel(\''+obj.btn_ele+'\',this)">&times;</span></li>')
+	$(obj.panel).find(".nav-tabs").append(li)
+	$(obj.panel).find(".tab-content").append('<div id="'+panel_id+'" class="tab-pane fade in"><div style="max-height: 300px;overflow-y: auto; overflow-x: auto;"><div class="log-content"></div></div></div>')
+	return li;
+}
+
+function closeLogPanel(btn,node){
+	targgleTab($(node).parents("ul").children().eq(0));
+	$($(node).parent().find("a").attr("href")).remove();
+	$(node).parents("li").remove()
+	clearBtnStatus(btn);
 }
 
 /**清除按钮状态与循环查询日志*/
 function clearBtnStatus(node){
-	clearInterval(loop)
-	$(node).text("提交")
+	loop=false;
+	$(node).text("执行")
 	$(node).data("status",0)
 }
 /**执行爬虫 配置事件*/
@@ -90,9 +142,10 @@ function btnExcete(param){
 					url:"/crawle/config/excute",
 					data:JSON.stringify(data),
 					type:"post",
+					loadding:true,
 					success:function(res){
 						if(res.success){
-							listenLog(res.data,node)
+							listenLog(res.data,node,param)
 							$(node).data("status",1)
 							$(node).text("放弃")
 						}else{
@@ -110,6 +163,5 @@ function btnExcete(param){
 		}else{
 			clearBtnStatus(node)
 		}
-		
 	})
 }
