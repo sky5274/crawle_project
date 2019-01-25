f81776231bf99d48b6793dbea995d1f47051bb4c
wangfan <tianlianyouren@163.com>
2019-01-25 09:43:33 +0800

    实例化  spring控制器的内核

--- web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
+++ web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
@@ -3,7 +3,10 @@ package com.crawl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.sky.pub.util.SpringUtil;
 
 @SpringBootApplication
 @ComponentScan(value= {"com.sky","com.crawl"})
@@ -11,7 +14,8 @@ import org.springframework.context.annotation.ComponentScan;
 public class WebCrawleApplication {
 
 	public static void main(String[] args) {
-		 SpringApplication.run(WebCrawleApplication.class, args);
+		 ConfigurableApplicationContext application = SpringApplication.run(WebCrawleApplication.class, args);
+		 SpringUtil.setApplicationContext(application);
 	}
 	
 }
