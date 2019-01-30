eca290ce4d10dd6a38b1a602725667c272be48e8
wangfan <tianlianyouren@163.com>
2019-01-30 14:25:54 +0800

    controller  方法aop监听

---
+++ core_pub/src/main/java/com/sky/pub/common/ControllAdvice.java
@@ -0,0 +1,96 @@
+package com.sky.pub.common;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import com.alibaba.fastjson.JSON;
+
+
+/** 
+ * 
+ * @author sky
+ * @description 主要用于调试control的过程
+ * <br><br>
+ * 把这个类声明为一个切面： 
+ * 1. 使用注解“@Component”把该类放入到IOC容器中 
+ * 2. 使用注解“@Aspect”把该类声明为一个切面 
+ *  
+ * 设置切面的优先级: 
+ * 3. 使用注解“@Order(number)”指定前面的优先级，值越小，优先级越高 
+ */  
+@Order(1)  
+@Aspect  
+@Component
+public class ControllAdvice {
+
+	private Log log=LogFactory.getLog(ControllAdvice.class);
+	private String prefix="-->>";
+	private Date times=null;
+	private Date timee=null;
+	String method=null;
+	@Pointcut("execution(* com.sky..*.controller.*.*(..))")
+	public void point(){}
+
+	@Before("point()")
+	public void before(JoinPoint joinpoint){
+		//获取request
+		times=new Date();
+		method=joinpoint.getSignature().getDeclaringTypeName()+"."+joinpoint.getSignature().getName();
+		HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		Map<String, Object> argMap=new HashMap<>();
+		MethodSignature msd = (MethodSignature) joinpoint.getSignature();
+
+		LocalVariableTableParameterNameDiscoverer u = new LocalVariableTableParameterNameDiscoverer();
+		String[] params = u.getParameterNames(msd.getMethod());
+		Object[] argList = joinpoint.getArgs();
+		for(int i=0;i<argList.length;i++){
+			if(argList[i] instanceof HttpServletRequest){
+				continue;
+			}else if(argList[i] instanceof HttpServletResponse){
+				continue;
+			}
+			argMap.put(params[i], argList[i]);
+		}
+		log.info(prefix+method+"  被"+req.getRemoteAddr()+"调用");
+		log.info(prefix+"url:"+req.getRequestURI()+"  args："+JSON.toJSONString(argMap));
+	}
+	@After("point()")
+	public synchronized void after(JoinPoint joinPoint){
+		timee=new Date();
+
+		try {
+			long cost=timee.getTime()-times.getTime();
+			log.info(prefix+joinPoint.getSignature().getName()+"  执行结束。 耗时:  "+cost+"ms;");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		timee=null;
+		times=null;
+	}
+
+	//	@AfterReturning(pointcut="point()",returning="val")
+	//	public void getReturn(JoinPoint joinpoint,Object val){
+	//		log.debug("方法："+joinpoint.getSignature().getName()+"  执行结果：   "+val);
+	//	}
+
+
+
+}
