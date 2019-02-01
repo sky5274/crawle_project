c7023ee463680c6c03234f490678c94b16a1f048
wangfan <tianlianyouren@163.com>
2019-02-01 20:14:29 +0800

    配置修改

--- web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
+++ web_crawle/src/main/java/com/crawl/WebCrawleApplication.java
@@ -3,10 +3,7 @@ package com.crawl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.sky.pub.util.SpringUtil;
 
 @SpringBootApplication
 @ComponentScan(value= {"com.sky","com.crawl"})
@@ -14,8 +11,7 @@ import com.sky.pub.util.SpringUtil;
 public class WebCrawleApplication {
 
 	public static void main(String[] args) {
-		 ConfigurableApplicationContext application = SpringApplication.run(WebCrawleApplication.class, args);
-		 SpringUtil.setApplicationContext(application);
+		 SpringApplication.run(WebCrawleApplication.class, args);
 	}
 	
 }
