774a0d9ba9b550d8494b1d2488f3c112227cd615
wangfan <tianlianyouren@163.com>
2019-02-19 13:11:10 +0800

    jobtask  controller 修改：页面view修改，查询任务的最新状态，以及调度节点查询方法

--- task_schedule/src/main/java/com/sky/task/controller/JobTaskController.java
+++ task_schedule/src/main/java/com/sky/task/controller/JobTaskController.java
@@ -2,21 +2,19 @@ package com.sky.task.controller;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.sky.pub.Page;
-import com.sky.pub.PageRequest;
 import com.sky.pub.Result;
 import com.sky.pub.ResultAssert;
 import com.sky.pub.ResultCode;
@@ -25,6 +23,7 @@ import com.sky.pub.common.exception.ResultException;
 import com.sky.pub.controller.BaseExcepionController;
 import com.sky.task.entity.JobTaskEntity;
 import com.sky.task.entity.TaskGroupEntity;
+import com.sky.task.entity.req.JobTaskReqData;
 import com.sky.task.service.JobTaskService;
 
 /**
@@ -41,31 +40,38 @@ public class JobTaskController extends BaseExcepionController{
 	private void validResult(BindingResult validResult) throws ResultException{
 		if(validResult.hasErrors()) {
 			List<String> msgs=new LinkedList<>();
-			for(FieldError field:validResult.getFieldErrors()) {
+			for(ObjectError field:validResult.getAllErrors()) {
 				msgs.add(field.getDefaultMessage());
 			}
+			System.err.println(msgs);
 			throw new ResultException(ResultCode.VALID, "定时任务校验："+String.join(";", msgs));
 		}
 	}
 	
+	@RequestMapping("/page/cron")
+	public ModelAndView taskQuartzCron() {
+		return new ModelAndView("/task/cron");
+	}
 	@RequestMapping("/page/list")
 	public ModelAndView taskListPage() {
-		ModelAndView view=new ModelAndView("tasklist");
+		ModelAndView view=new ModelAndView("/task/list");
+		view.addObject("groups", jobTaskService.queryGroup());
 		return view;
 	}
 	@RequestMapping("/page/cart")
 	public ModelAndView taskCartPage(Integer id) {
-		ModelAndView view=new ModelAndView("taskcart");
+		ModelAndView view=new ModelAndView("/task/cart");
 		view.addObject("job", jobTaskService.getJobTask(id));
 		return view;
 	}
 	@RequestMapping("/page/modify")
 	public ModelAndView taskModifyPage(HttpServletRequest req) {
-		ModelAndView view=new ModelAndView("taskmodify");
+		ModelAndView view=new ModelAndView("/task/modify");
 		String id=req.getParameter("id");
 		if(!StringUtils.isEmpty(id)) {
 			view.addObject("job", jobTaskService.getJobTask(Integer.valueOf(id)));
 		}
+		view.addObject("groups", jobTaskService.queryGroup());
 		return view;
 	}
 	
@@ -87,11 +93,20 @@ public class JobTaskController extends BaseExcepionController{
 		return ResultUtil.getOk( jobTaskService.cancelJobTask(job));
 	}
 	
-	@RequestMapping(value="/query/page",method=RequestMethod.POST)
-	public Result<Page<JobTaskEntity>> queryPageTaskJob(@RequestBody PageRequest<JobTaskEntity> jobPage){
+	@RequestMapping(value="/query/task")
+	public Result<JobTaskEntity> queryTaskJob(int id){
+		return ResultUtil.getOk( jobTaskService.getJobTask(id));
+	}
+	
+	@RequestMapping(value="/query/page")
+	public Result<Page<JobTaskEntity>> queryPageTaskJob(JobTaskReqData jobPage){
 		return ResultUtil.getOk( jobTaskService.queryPageByTask(jobPage));
 	}
 	
+	@RequestMapping(value="/query/node")
+	public Result<Set<String>> queryJobNodeUrl(String groupId,String className){
+		return ResultUtil.getOk( jobTaskService.getNodeIps(groupId,className));
+	}
 	
 	@RequestMapping(value="/group/add",method=RequestMethod.POST)
 	public Result<TaskGroupEntity> addTaskGroup(@RequestBody TaskGroupEntity group) throws ResultException {
@@ -114,19 +129,19 @@ public class JobTaskController extends BaseExcepionController{
 	}
 	
 	@RequestMapping(value="/task/run",method=RequestMethod.POST)
-	public void runJobTask(@RequestBody @Valid JobTaskEntity job,BindingResult validResult) throws ResultException {
+	public Result<Boolean> runJobTask(@RequestBody @Valid JobTaskEntity job,BindingResult validResult) throws ResultException {
 		validResult(validResult);
 		ResultAssert.isBlank(job.getTaskId(), "调度任务编码不允许为空");
-		jobTaskService.runJobTask(job);
+		return ResultUtil.getOk(jobTaskService.runJobTask(job));
 	}
 	@RequestMapping(value="/task/pause",method=RequestMethod.POST)
-	public void pauseJobTask(@RequestBody JobTaskEntity job) throws ResultException {
+	public Result<Boolean> pauseJobTask(@RequestBody JobTaskEntity job) throws ResultException {
 		ResultAssert.isBlank(job.getTaskId(), "调度任务编码不允许为空");
-		jobTaskService.pauseJobTask(job);
+		return ResultUtil.getOk(jobTaskService.pauseJobTask(job));
 	}
 	@RequestMapping(value="/task/close",method=RequestMethod.POST)
-	public void closeJobTask(@RequestBody JobTaskEntity job) throws ResultException {
+	public Result<Boolean> closeJobTask(@RequestBody JobTaskEntity job) throws ResultException {
 		ResultAssert.isBlank(job.getTaskId(), "调度任务编码不允许为空");
-		jobTaskService.closeJobTask(job);
+		return ResultUtil.getOk(jobTaskService.closeJobTask(job));
 	}
 }
