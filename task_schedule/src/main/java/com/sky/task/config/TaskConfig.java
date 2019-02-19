1c62516906cdd14f5b73cd4e7d6ad374c0bf9faa
wangfan <tianlianyouren@163.com>
2019-02-19 13:08:45 +0800

    添加SchedulerFactoryBean  bean primary

--- task_schedule/src/main/java/com/sky/task/config/TaskConfig.java
+++ task_schedule/src/main/java/com/sky/task/config/TaskConfig.java
@@ -1,15 +1,32 @@
 package com.sky.task.config;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.hibernate.validator.HibernateValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 @Configuration
 public class TaskConfig {
 	
 	@Bean
+	@Primary
 	public SchedulerFactoryBean getJobConfig() {
 		return new SchedulerFactoryBean();
+	}
+	
+	@Bean
+	public Validator getValidate() {
+		ValidatorFactory validatorFactory = Validation.byProvider( HibernateValidator.class )
+                .configure()
+                .addProperty( "hibernate.validator.fail_fast", "false" )
+                .buildValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        return validator;
 	}
 	
 }
