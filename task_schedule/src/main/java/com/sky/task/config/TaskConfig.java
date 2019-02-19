6d8fa682d7aa6a63c89ed763732252f41bde4c43
wangfan <tianlianyouren@163.com>
2019-02-19 13:31:45 +0800

    多余公共静态文件修改

--- task_schedule/src/main/java/com/sky/task/config/TaskConfig.java
+++ task_schedule/src/main/java/com/sky/task/config/TaskConfig.java
@@ -1,10 +1,5 @@
 package com.sky.task.config;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-
-import org.hibernate.validator.HibernateValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -15,18 +10,8 @@ public class TaskConfig {
 	
 	@Bean
 	@Primary
-	public SchedulerFactoryBean getJobConfig() {
+	public SchedulerFactoryBean getSchedulerFactoryBean() {
 		return new SchedulerFactoryBean();
-	}
-	
-	@Bean
-	public Validator getValidate() {
-		ValidatorFactory validatorFactory = Validation.byProvider( HibernateValidator.class )
-                .configure()
-                .addProperty( "hibernate.validator.fail_fast", "false" )
-                .buildValidatorFactory();
-        Validator validator = validatorFactory.getValidator();
-        return validator;
 	}
 	
 }
