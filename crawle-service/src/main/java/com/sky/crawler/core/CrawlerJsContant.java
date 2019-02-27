c2069d1614cf76bf08639d59ce90ca316ef44970
wangfan <tianlianyouren@163.com>
2019-02-27 15:07:51 +0800

    crawler  文件路径静态方法

--- crawle-service/src/main/java/com/sky/crawler/core/CrawlerJsContant.java
+++ crawle-service/src/main/java/com/sky/crawler/core/CrawlerJsContant.java
@@ -1,5 +1,7 @@
 package com.sky.crawler.core;
 
+import java.io.InputStream;
+
 public class CrawlerJsContant {
 	/**java8  javaScript  初始话文件*/
 	public static String initJs="/js/init.evn.js";
@@ -8,5 +10,21 @@ public class CrawlerJsContant {
 	/**获取资源文件的实际地址*/
 	public static String getFilePath(String path) {
 		return CrawlerJsContant.class.getResource(path).getFile();
+	}
+	public static String getJarPath() {
+		String path = CrawlerJsContant.class.getResource("/").getFile();
+		if(path.contains("!/")) {
+			int startIndex=0;
+			if(path.startsWith("file:/")) {
+				startIndex=6;
+			}
+			int lastIndex = path.substring(0, path.indexOf("!/")).lastIndexOf("/");
+			path=path.substring(startIndex,lastIndex);
+		}
+		return path;
+	}
+	/**获取资源文件的数据流*/
+	public static InputStream getFilePathStream(String path) {
+		return CrawlerJsContant.class.getResourceAsStream(path);
 	}
 }
