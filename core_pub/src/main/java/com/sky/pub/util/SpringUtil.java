3ce8df6ba97fcd33a499b3193131d0da04d89996
wangfan <tianlianyouren@163.com>
2019-03-08 16:15:08 +0800

    spring工具废分添加查询配置属性的方法

--- core_pub/src/main/java/com/sky/pub/util/SpringUtil.java
+++ core_pub/src/main/java/com/sky/pub/util/SpringUtil.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 
 @Configuration 
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -27,7 +28,6 @@ public class SpringUtil implements ApplicationContextAware{
     //通过name获取 Bean.
     public static Object getBean(String name){
         return getApplicationContext().getBean(name);
- 
     }
  
     //通过class获取Bean.
@@ -38,5 +38,21 @@ public class SpringUtil implements ApplicationContextAware{
     //通过name,以及Clazz返回指定的Bean
     public static <T> T getBean(String name,Class<T> clazz){
         return getApplicationContext().getBean(name, clazz);
+    }
+    
+    public static Environment getEvn() {
+    	return getApplicationContext().getEnvironment();
+    }
+    public static String getEvnProperty(String key) {
+    	return getEvn().getProperty(key);
+    }
+    public static String getEvnProperty(String key,String defaultValue) {
+    	return getEvn().getProperty(key,defaultValue);
+    }
+    public static <T> T getEvnProperty(String key,Class<T> targetType) {
+		return getEvn().getProperty(key,targetType);
+    }
+    public static <T> T getEvnProperty(String key,Class<T> targetType,T defaultValue) {
+    	return getEvn().getProperty(key,targetType,defaultValue);
     }
 }
