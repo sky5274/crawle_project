60e70a97870facade074f5aa19a6d4787895ad73
wangfan <tianlianyouren@163.com>
2019-03-08 16:14:42 +0800

    统一异常处理修改

--- core_pub/src/main/java/com/sky/pub/controller/BaseExcepionController.java
+++ core_pub/src/main/java/com/sky/pub/controller/BaseExcepionController.java
@@ -1,18 +1,23 @@
 package com.sky.pub.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import com.sky.pub.Result;
 import com.sky.pub.ResultCode;
+import com.sky.pub.common.SpringHandlerExceptionResolver;
 import com.sky.pub.common.exception.ResultException;
 @SuppressWarnings("rawtypes")
 public class BaseExcepionController {
+	protected Log log=LogFactory.getLog(getClass());
 	@ExceptionHandler(ResultException.class)
 	public Result<?> getException(ResultException exp){
 		return new Result(exp.getCode(), exp.getMsg()).fail();
 	}
+	@SuppressWarnings("unchecked")
 	@ExceptionHandler(Exception.class)
 	public Result<?> getException(Exception exp){
-		return new Result(ResultCode.UNKONW_EXCEPTION).fail();
+		log.error(exp.getMessage(), exp);
+		return new Result(ResultCode.UNKONW_EXCEPTION,SpringHandlerExceptionResolver.getExceptionStrace(exp)).fail();
 	}
 }
