01e6c9bd12edc67c23528eb209e78a59f10fae79
wangfan <tianlianyouren@163.com>
2019-04-08 16:20:28 +0800

    使用自定义 的springboot启动注解

--- task_schedule/src/main/java/com/sky/task/TaskApplication.java
+++ task_schedule/src/main/java/com/sky/task/TaskApplication.java
@@ -2,13 +2,11 @@ package com.sky.task;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
+import com.sky.pub.annotation.SpringStartApplication;
 
-@SpringBootApplication
 @EnableEurekaClient
-@ComponentScan(value= {"com.sky"})
+@SpringStartApplication
 @MapperScan(value= {"com.sky.*.dao"})
 public class TaskApplication {
 	public static void main(String[] args) {
