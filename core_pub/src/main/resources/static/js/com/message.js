e0395ae5f847bf8634d5d0c17146e9fa437e971b
wangfan <tianlianyouren@163.com>
2019-05-07 16:47:05 +0800

    dialog关闭时执行func方法问题修复

--- core_pub/src/main/resources/static/js/com/message.js
+++ core_pub/src/main/resources/static/js/com/message.js
@@ -101,7 +101,7 @@
 				'<div class="modal-dialog">'+
 				' <div class="modal-content">'+
 				' <div class="modal-header">'+
-				'     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>'+
+				'     <button class="close" data-dismiss="modal" aria-hidden="true">&times;</button>'+
 				'    <h4 class="modal-title" id="myModalLabel">'+this.settings.title+'</h4>'+
 				' </div>'+
 				' <div class="modal-body">'+this.settings.con+'</div>'+
