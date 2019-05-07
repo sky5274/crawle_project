100f73053a2fe6176be73d25c5bf74979dfdde10
wangfan <tianlianyouren@163.com>
2019-05-07 17:03:57 +0800

    spring auth 启动类修改

--- config_auth/src/main/java/com/sky/auth/AuthConfigApplication.java
+++ config_auth/src/main/java/com/sky/auth/AuthConfigApplication.java
@@ -2,13 +2,11 @@ package com.sky.auth;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
+import com.sky.pub.annotation.SpringStartApplication;
 
-@SpringBootApplication
+@SpringStartApplication
 @EnableEurekaClient
-@ComponentScan(value= {"com.sky"})
 @MapperScan(value= {"com.sky.auth.config.dao"})
 public class AuthConfigApplication {
 	public static void main(String[] args) {
