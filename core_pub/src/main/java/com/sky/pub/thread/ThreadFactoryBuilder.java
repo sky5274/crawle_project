80255e24601549013719730ee18acc95b2a14d6e
wangfan <tianlianyouren@163.com>
2019-05-07 16:45:04 +0800

    线程创建工厂名称变更

--- core_pub/src/main/java/com/sky/pub/thread/ThreadFactoryBuilder.java
+++ core_pub/src/main/java/com/sky/pub/thread/ThreadFactoryBuilder.java
@@ -14,31 +14,36 @@ import java.util.concurrent.ThreadFactory;
  */
 public class ThreadFactoryBuilder implements ThreadFactory {
 
-	 private long counter;  
-	    private String name;  
-	    private List<String> stats; 
+	private long counter;  
+	private String name;  
+	private String prefix="crwale_";  
+	private List<String> stats; 
 	public ThreadFactoryBuilder(String name) {
+		this("crwale_",name);
+	}
+	public ThreadFactoryBuilder(String prefix,String name) {
 		this.name=name;
+		this.prefix=prefix;
 		this.counter=0;
 		stats=new LinkedList<>();
 	}
 	@Override  
-    public Thread newThread(Runnable r) {  
-        Thread t = new Thread(r,"qsyhxc_"+name+"_"+counter);  
-        counter++;  
-        stats.add(String.format("Created thread %d with name %s on %s\n", t.getId(),t.getName(),new Date()));  
-        return t;  
-    }  
+	public Thread newThread(Runnable r) {  
+		Thread t = new Thread(r,prefix+name+"_"+counter);  
+		counter++;  
+		stats.add(String.format("Created thread %d with name %s on %s\n", t.getId(),t.getName(),new Date()));  
+		return t;  
+	}  
 
-	
+
 	public String getStatuts(){  
-        StringBuffer buffer = new StringBuffer();  
-        Iterator<String> it = stats.iterator();  
-        while(it.hasNext()){  
-            buffer.append(it.next());  
-            buffer.append("\n");  
-        }  
-        return buffer.toString();  
-    }
-	
+		StringBuffer buffer = new StringBuffer();  
+		Iterator<String> it = stats.iterator();  
+		while(it.hasNext()){  
+			buffer.append(it.next());  
+			buffer.append("\n");  
+		}  
+		return buffer.toString();  
+	}
+
 }
