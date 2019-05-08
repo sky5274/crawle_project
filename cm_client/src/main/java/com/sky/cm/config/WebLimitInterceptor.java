633704f6d9a92a76357e0f1eb3587f159e8f046f
wangfan <tianlianyouren@163.com>
2019-05-08 16:02:16 +0800

    限流客户端：限流方案修改：添加服务限流是否使用判断，以及限流措施层级判断等

--- cm_client/src/main/java/com/sky/cm/config/WebLimitInterceptor.java
+++ cm_client/src/main/java/com/sky/cm/config/WebLimitInterceptor.java
@@ -3,22 +3,24 @@ package com.sky.cm.config;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import com.alibaba.fastjson.JSON;
+import com.sky.cm.annotation.Limit;
 import com.sky.cm.bean.LimitBean;
 import com.sky.cm.core.RequestLimitBeanRedisServiceImpl;
 import com.sky.cm.core.SkyConfig;
 import com.sky.cm.core.SkyConfigValue;
 import com.sky.pub.Result;
-import com.sky.pub.common.exception.ResultException;
 
 @Component
 public class WebLimitInterceptor implements HandlerInterceptor {
@@ -27,6 +29,20 @@ public class WebLimitInterceptor implements HandlerInterceptor {
 	@Autowired
 	private RequestLimitBeanRedisServiceImpl requestLimitService;
 	
+	private static List<String> resourceUrlEndWithList=Arrays.asList(".js",".css",".img",".png",".jpeg",".xls",".word");
+	
+	private boolean isResourceUrl(String url) {
+		boolean flag=false;
+		if(!StringUtils.isEmpty(url)) {
+			for(String endWith:resourceUrlEndWithList) {
+				flag=url.endsWith(endWith);
+				if(flag) {
+					break;
+				}
+			}
+		}
+		return flag;
+	}
 	private String getKey(HttpServletRequest req) {
 		SkyConfigValue config = skyConfig.getConfig();
 		StringBuilder key=new StringBuilder();
@@ -39,19 +55,34 @@ public class WebLimitInterceptor implements HandlerInterceptor {
 	private List<String> unLimitUrl=Arrays.asList("/error");
 	
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handlerMethod)
 			throws Exception {
-		if(unLimitUrl.contains(request.getRequestURI()) || request.getRequestURI().toString().endsWith(".js")) {
+		if(unLimitUrl.contains(request.getRequestURI()) || isResourceUrl(request.getRequestURI().toString())) {
 			return true;
 		}
-		LimitBean limit = requestLimitService.isLimit(getKey(request));
+		Limit limitdef=null;
+		if(handlerMethod instanceof HandlerMethod ){
+			if (handlerMethod != null) {
+				HandlerMethod handler = (HandlerMethod)handlerMethod;
+				Method method = handler.getMethod();
+				if (method != null) {
+					//如果定义了ExceptionHandler则返回相应的Map中的数据
+					limitdef = method.getAnnotation(Limit.class);
+				}
+			}
+		}
+		//服务限流：false 或限流注解无时，不限流
+		if(!skyConfig.getConfig().isEnablelimit() && limitdef==null) {
+			return true;
+		}
+		LimitBean limit = requestLimitService.isLimit(getKey(request),limitdef);
 		if(limit.isLimit()) {
 			response.setCharacterEncoding("UTF-8");  
 		    response.setContentType("application/json; charset=utf-8");  
 		    PrintWriter out = null;  
 		    try {  
 		        out = response.getWriter();  
-		        out.write(JSON.toJSONString(new Result<>("l-1", String.format("网络限流: %dtimes >> %d/s",limit.getLimitCount(),limit.getPriod()),false)));
+		        out.write(JSON.toJSONString(new Result<>("l-1", String.format("网络限流: %d/%d  (times/s)",limit.getLimitCount(),limit.getPriod()),false)));
 		        out.flush();
 		    } catch (IOException e) {  
 		    } finally {  
--- cm_client/src/main/java/com/sky/cm/core/RequestLimitBeanRedisServiceImpl.java
+++ cm_client/src/main/java/com/sky/cm/core/RequestLimitBeanRedisServiceImpl.java
@@ -2,13 +2,17 @@ package com.sky.cm.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import com.sky.cm.annotation.Limit;
 import com.sky.cm.bean.LimitBean;
+import com.sky.cm.enums.LimitType;
 
 @Component
 public class RequestLimitBeanRedisServiceImpl extends BaseRedisServiceImpl<LimitBean>{
 	@Autowired
 	private SkyConfig skyConfig;
+	private static String spiteStr="_";
 
 	@Override
 	protected Class<LimitBean> getExtendClass() {
@@ -30,9 +34,23 @@ public class RequestLimitBeanRedisServiceImpl extends BaseRedisServiceImpl<Limit
 	 * @param key
 	 * @return
 	 * @author 王帆
+	 * @param limitdef 
 	 * @date 2019年3月13日 上午9:27:23
 	 */
-	public LimitBean isLimit(String key) {
+	public LimitBean isLimit(String key, Limit limitdef) {
+		if(limitdef !=null) {
+			StringBuilder temp=new StringBuilder();
+			if(!StringUtils.isEmpty(limitdef.prefix())) {
+				temp.append(limitdef.prefix()).append(spiteStr);
+			}
+			if(!StringUtils.isEmpty(limitdef.key())) {
+				temp.append(limitdef.key()).append(spiteStr);
+			}
+			if(!StringUtils.isEmpty(limitdef.name())) {
+				temp.append(limitdef.name()).append(spiteStr);
+			}
+			key=temp.toString()+key;
+		}
 		LimitBean limit = doStringGet(key);
 		if(limit != null) {
 			if(limit.isLimit()) {
@@ -44,8 +62,25 @@ public class RequestLimitBeanRedisServiceImpl extends BaseRedisServiceImpl<Limit
 			}
 		}else {
 			//查询配置信息
+			/*
+			 * 1:LimitType.LOCATION  使用本地限流配置
+			 * 2：服务限流标识为true  使用服务限流配置
+			 * 3: 服务限流标识为false  limit存在  使用本地限流配置
+			 */
 			String url =key.substring(key.indexOf("$")+1);
-			limit=skyConfig.getLimit(url);
+			if(limitdef !=null && limitdef.limitType()==LimitType.LOCATION) {
+				limit=new LimitBean(key);
+				limit.setCount(limitdef.count());
+				limit.setPriod(limitdef.period());
+				limit.setLimitCount(limitdef.count());
+			}else if(skyConfig.getConfig().isEnablelimit()){
+				limit=skyConfig.getLimit(url);
+			}else if(limitdef !=null){
+				limit=new LimitBean(key);
+				limit.setCount(limitdef.count());
+				limit.setPriod(limitdef.period());
+				limit.setLimitCount(limitdef.count());
+			}
 			if(limit==null) {
 				limit=new LimitBean(key);
 			}
