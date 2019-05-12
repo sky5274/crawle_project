4e852bf096896eacd33927a8a08d0c361315a0e2
wangfan <tianlianyouren@163.com>
2019-05-12 19:38:43 +0800

    限流-属性测试服务修改

--- demo_client_controller/src/main/java/com/sky/demo/controller/PageController.java
+++ demo_client_controller/src/main/java/com/sky/demo/controller/PageController.java
@@ -2,17 +2,22 @@ package com.sky.demo.controller;
 
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import com.sky.cm.annotation.Limit;
+import com.sky.cm.core.SkyConfig;
 import com.sky.pub.Result;
 import com.sky.pub.ResultUtil;
 
 @RestController
 public class PageController {
+	@Autowired
+	SkyConfig skyconfig;
 	
 	@RequestMapping("/page/{name}")
 	public ModelAndView goPage(@PathVariable String name) {
@@ -27,6 +32,10 @@ public class PageController {
 	@RequestMapping("/talk")
 	public Result<String> talk(String word) {
 		return ResultUtil.getOk("you talk:"+word);
+	}
+	@RequestMapping("/property")
+	public Result<String> getproperty(String key) {
+		return ResultUtil.getOk(skyconfig.getProperty(key));
 	}
 	@RequestMapping("/writer")
 	public Result<Map<String, String[]>> say(HttpServletRequest req) {
