b1f1ef42dc8476d1e99ea04ad4e184853459eb44
wangfan <tianlianyouren@163.com>
2019-05-14 14:35:32 +0800

    限流-属性服务器controller添加注释

--- sevice_manager/src/main/java/com/sky/sm/controller/HttpServiceController.java
+++ sevice_manager/src/main/java/com/sky/sm/controller/HttpServiceController.java
@@ -24,21 +24,51 @@ public class HttpServiceController {
 	@Autowired
 	private TraceLimitService traceLimitService;
 	
+	/**
+	 * url限流配置查询请求
+	 * @param limit
+	 * @return
+	 * @author 王帆
+	 * @date 2019年5月14日 下午1:46:30
+	 */
 	@RequestMapping("limit")
 	public Result<TraceLimitBean> limit(TraceLimitReqBean limit) {
 		return ResultUtil.getOk(traceLimitService.queryMaxLike(limit));
 	}
 	
+	/**
+	 * 链路追踪--开始
+	 * @param trace
+	 * @return
+	 * @throws ResultException
+	 * @author 王帆
+	 * @date 2019年5月14日 下午1:46:59
+	 */
 	@RequestMapping("/trace/start")
 	public Result<Boolean> traceStart(TraceRecordEntity trace) throws ResultException {
 		return ResultUtil.getOk(traceRecordSerive.saveFast(trace));
 	}
 	
+	/**
+	 * 链路追踪--结束
+	 * @param trace
+	 * @return
+	 * @throws ResultException
+	 * @author 王帆
+	 * @date 2019年5月14日 下午1:47:19
+	 */
 	@RequestMapping("/trace/end")
 	public  Result<Boolean> traceEnd(TraceRecordEntity trace) throws ResultException {
 		return ResultUtil.getOk(traceRecordSerive.updateFast(trace));
 	}
 	
+	/**
+	 * 链路追踪--查询
+	 * @param trace
+	 * @return
+	 * @author 王帆
+	 * @date 2019年5月14日 下午1:47:35
+	 */
 	@RequestMapping("/query/trace")
 	public Result<List<TraceRecordBean>> queryTrace(TraceRecordEntity trace){
 		return ResultUtil.getOk(traceRecordSerive.queryTrace(trace));
