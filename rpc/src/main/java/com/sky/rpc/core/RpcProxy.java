6dfe9ba2c28d90626e8dbbb716008d353b23244a
wangfan <tianlianyouren@163.com>
2019-07-10 17:23:11 +0800

    rpc clazz proxy modify

--- rpc/src/main/java/com/sky/rpc/core/RpcProxy.java
+++ rpc/src/main/java/com/sky/rpc/core/RpcProxy.java
@@ -6,6 +6,8 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.net.InetSocketAddress;
 
+import com.sky.rpc.core.cilent.RpcClient;
+
 /**
  * rpc client proxy
  *<p>Title: RpcProxy.java</p>
@@ -16,22 +18,11 @@ import java.net.InetSocketAddress;
  * @date 2018年10月19日
  */
 public class RpcProxy{
-	@SuppressWarnings("unchecked")
 	public static <T> T getRemoteProxyObj(final Class<T> serviceInterface, final InetSocketAddress addr) {
 		// 1.将本地的接口调用转换成JDK的动态代理，在动态代理中实现接口的远程调用
-		return (T) Proxy.newProxyInstance(RpcProxy.class.getClassLoader(), new Class<?>[]{serviceInterface},
-				new InvocationHandler() {
-					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-						try {
-							return new RpcClient<T>(addr).request(serviceInterface,method,args,null);
-						} catch (InvocationTargetException e){
-							//抛出造成的异常
-							throw e.getCause();
-						}
-					}
-
-		});
+		return getRemoteProxyObj(serviceInterface,addr,null);
 	}
+	
 	@SuppressWarnings("unchecked")
 	public static <T> T getRemoteProxyObj(final Class<T> serviceInterface, final InetSocketAddress addr,final String interfaceImpl) {
 		// 1.将本地的接口调用转换成JDK的动态代理，在动态代理中实现接口的远程调用
@@ -40,6 +31,22 @@ public class RpcProxy{
 			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 				try {
 					return new RpcClient<T>(addr).request(serviceInterface,method,args,interfaceImpl);
+				} catch (InvocationTargetException e){
+					//抛出造成的异常
+					throw e.getCause();
+				}
+			}
+			
+		});
+	}
+	@SuppressWarnings("unchecked")
+	public static <T> T getRemoteProxyObj(final Class<T> serviceInterface, final InetSocketAddress addr,final String interfaceImpl,final int timeout) {
+		// 1.将本地的接口调用转换成JDK的动态代理，在动态代理中实现接口的远程调用
+		return (T) Proxy.newProxyInstance(RpcProxy.class.getClassLoader(), new Class<?>[]{serviceInterface},
+				new InvocationHandler() {
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				try {
+					return new RpcClient<T>(addr,timeout).request(serviceInterface,method,args,interfaceImpl);
 				} catch (InvocationTargetException e){
 					//抛出造成的异常
 					throw e.getCause();
