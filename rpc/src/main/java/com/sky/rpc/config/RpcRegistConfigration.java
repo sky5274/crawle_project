bafa29a1e45ba36131d9d23e49808a401d98e539
wangfan <tianlianyouren@163.com>
2019-07-11 11:39:38 +0800

    rpc auto config

---
+++ rpc/src/main/java/com/sky/rpc/config/RpcRegistConfigration.java
@@ -0,0 +1,38 @@
+package com.sky.rpc.config;
+
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * rpc 配置服务
+ * @author 王帆
+ * @date  2019年7月9日 下午4:10:25
+ */
+public class RpcRegistConfigration implements ImportBeanDefinitionRegistrar{
+	@Override
+	public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+		 // 注册rpc netty  closed listener
+		 registBean(ApplicationNettyClosedListener.class,registry);
+		 
+		 //rpc provider\consumer  regist aciotn
+		 RpcReadyRegistInfoConfigration config=new RpcReadyRegistInfoConfigration();
+		 config.setRegistry(registry);
+		 config.init();
+	}
+	
+	private GenericBeanDefinition registBean( Class<?> intertface, BeanDefinitionRegistry registry) {
+		return registBean(intertface.getSimpleName(),intertface,registry);
+	}
+	private GenericBeanDefinition registBean(String id, Class<?> intertface, BeanDefinitionRegistry registry) {
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(intertface);
+		GenericBeanDefinition definition = (GenericBeanDefinition) builder.getRawBeanDefinition();
+		definition.setAutowireMode(GenericBeanDefinition.AUTOWIRE_BY_TYPE);
+		registry.registerBeanDefinition(id,definition);
+		return definition;
+	}
+
+}
