47bfe4ca372cd4ddecb1656d692f51d32db69447
wangfan <tianlianyouren@163.com>
2019-08-18 21:06:11 +0800

    删除多余配置

--- core_pub/src/main/java/com/sky/pub/web/config/SessionConfiguration.java
+++ core_pub/src/main/java/com/sky/pub/web/config/SessionConfiguration.java
@@ -1,14 +1,12 @@
 package com.sky.pub.web.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import com.sky.pub.web.interceptor.SessionInterceptor;
 
 
 @Configuration
-@EnableRedisHttpSession
 public class SessionConfiguration extends WebMvcConfigurerAdapter{
 	
     @Override
