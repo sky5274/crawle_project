3bd3d5c1b7442548dc91782a98f35a792507379e
wangfan <tianlianyouren@163.com>
2019-08-23 14:01:44 +0800

    字符串输出js

--- core_pub/src/main/resources/static/js/com/tool.js
+++ core_pub/src/main/resources/static/js/com/tool.js
@@ -571,3 +571,20 @@ function date(format, timestamp){
         return ret; 
     }); 
 }
+
+function sprintf(str) {
+    var args = arguments,
+        flag = true,
+        i = 1;
+
+    str = str.replace(/%s/g, function () {
+        var arg = args[i++];
+
+        if (typeof arg === 'undefined') {
+            flag = false;
+            return '';
+        }
+        return arg;
+    });
+    return flag ? str : '';
+};
