38a1bf268a5c2554c959b2cf894a1a98ae8334c7
wangfan <tianlianyouren@163.com>
2019-10-08 15:49:39 +0800

    string json redis service add docs

--- sevice_manager/src/main/java/com/sky/sm/service/impl/StringJsonRedisServiceImpl.java
+++ sevice_manager/src/main/java/com/sky/sm/service/impl/StringJsonRedisServiceImpl.java
@@ -10,6 +10,11 @@ import org.springframework.stereotype.Component;
 import com.alibaba.fastjson.JSON;
 import com.sky.pub.service.impl.BaseRedisServiceImpl;
 
+/**
+ * jsonString  redis service
+ * @author 王帆
+ * @date  2019年10月8日 下午3:21:43
+ */
 @Component
 public class StringJsonRedisServiceImpl extends BaseRedisServiceImpl<String>{
 
