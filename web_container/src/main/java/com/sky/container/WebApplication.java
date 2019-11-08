54e0a505010132cf23da65e663fcf3496aa973fb
wangfan <tianlianyouren@163.com>
2019-11-08 16:38:15 +0800

    remove cloud config

--- web_container/src/main/java/com/sky/container/WebApplication.java
+++ web_container/src/main/java/com/sky/container/WebApplication.java
@@ -2,11 +2,8 @@ package com.sky.container;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 import com.sky.pub.annotation.SpringStartApplication;
 
-@EnableEurekaClient
 @SpringStartApplication
 @MapperScan(value= {"com.sky.*.dao"})
 public class WebApplication {
