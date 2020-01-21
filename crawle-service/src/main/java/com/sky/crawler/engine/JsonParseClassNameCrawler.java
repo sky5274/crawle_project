bd538960744d590e6b4fdf04c6919c5083a8a98e
wangfan <wangfan@microvideo.cn>
2020-01-21 10:45:38 +0800

    crawler 服务引擎修改添加 crawlerservice 修改

--- crawle-service/src/main/java/com/sky/crawler/engine/JsonParseClassNameCrawler.java
+++ crawle-service/src/main/java/com/sky/crawler/engine/JsonParseClassNameCrawler.java
@@ -1,17 +1,13 @@
 package com.sky.crawler.engine;
 
 import java.io.Writer;
-import java.util.LinkedList;
-import java.util.List;
-
 import com.sky.crawler.service.CrawlerJsonService;
+import com.sky.crawler.service.CrawlerServiceFactoryUtil;
 import com.sky.pub.ResultCode;
 import com.sky.pub.common.exception.ResultException;
-import com.sky.pub.util.SpringUtil;
 
 public class JsonParseClassNameCrawler extends JsonParseClassCrawler{
 	private String className;
-	private Class<CrawlerJsonService> superClass=CrawlerJsonService.class;
 	
 	public JsonParseClassNameCrawler(String className) throws ResultException {
 		super();
@@ -28,52 +24,11 @@ public class JsonParseClassNameCrawler extends JsonParseClassCrawler{
 
 	
 	public JsonParseClassNameCrawler bulid() throws ResultException {
-		if(className==null) {
-			throw new ResultException(ResultCode.PARAM_VALID,"请确认调用的java Class");
-		}
-		try {
-			Class<?> clazz = Class.forName(className);
-			if(!isCrawlerSerivce(clazz)) {
-				throw new ResultException(ResultCode.PARAM_VALID,"调用的 "+className+" is not extends "+superClass);
-			}
-			CrawlerJsonService crawlerService=(CrawlerJsonService) SpringUtil.getBean(clazz);
-			super.setCrawlerService(crawlerService);
-		} catch (ClassNotFoundException  e) {
-			throw new ResultException(ResultCode.PARAM_VALID,className+"对应类不在系统中",e);
+		this.setCrawlerService(CrawlerServiceFactoryUtil.getClassBean(className, CrawlerJsonService.class));
+		if(this.getCrawlerService()==null) {
+			throw new ResultException(ResultCode.UNKONW_EXCEPTION, "未确定 CrawlerJsonService 服务");
 		}
 		return this;
-	}
-	
-	/**
-	 * 判定是否继承
-	 * @param clazz
-	 * @return
-	 * @author 王帆
-	 * @date 2019年1月18日 下午3:02:49
-	 */
-	private boolean isCrawlerSerivce(Class<?> clazz) {
-		boolean flag=false;
-		List<Class<?>> superClasses = getAllSuperClass(clazz);
-		for(Class<?> sup:superClasses) {
-			if(sup.getName().equals(superClass.getName())) {
-				flag=true;
-			}
-		}
-		return flag;
-	}
-	
-	private List<Class<?>> getAllSuperClass(Class<?> clazz) {
-		List<Class<?>> supers=new LinkedList<Class<?>>();
-		return getAllSuperClass(clazz,supers);
-	}
-	
-	private List<Class<?>> getAllSuperClass(Class<?> clazz, List<Class<?>> supers) {
-		if(!Object.class.getName().equals(clazz.getName())) {
-			Class<?> superClass = clazz.getSuperclass();
-			supers.add(superClass);
-			return getAllSuperClass(superClass,supers);
-		}
-		return supers;
 	}
 	
 	public String getClassName() {
