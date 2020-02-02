68810d9b4f0eb6727cfd32a4759d230899a5389d
wangfan <wangfan@microvideo.cn>
2020-02-02 21:46:44 +0800

    获取本地ip方法

--- core_pub/src/main/java/com/sky/pub/util/ComPubUtil.java
+++ core_pub/src/main/java/com/sky/pub/util/ComPubUtil.java
@@ -1,10 +1,48 @@
 package com.sky.pub.util;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 import java.util.UUID;
+import org.springframework.util.StringUtils;
 
 public class ComPubUtil {
+	private static String ip;
 	
 	public static String getUuid() {
 		return UUID.randomUUID().toString().replaceAll("-","");
+	}
+	
+	/**
+	 * 获取本地ip
+	 * @return
+	 * @author wangfan
+	 * @date 2020年1月29日 下午8:08:06
+	 */
+	public static String getIp() {
+		if(StringUtils.isEmpty(ip)) {
+			// 获取ip地址
+			try {
+				Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+				InetAddress ip = null;
+				while (allNetInterfaces.hasMoreElements()) {
+					NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+					if (netInterface.isLoopback() || netInterface.isVirtual() || !netInterface.isUp()) {
+						continue;
+					} else {
+						Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+						while (addresses.hasMoreElements()) {
+							ip = addresses.nextElement();
+							if (ip != null && ip instanceof Inet4Address) {
+								return ip.getHostAddress();
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+			}
+		}
+		return ip;
 	}
 }
